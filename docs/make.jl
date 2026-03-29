96f5699ec6c048bd74290704bb53bfa984e674f0
Markus Gumbel <m.gumbel@hs-mannheim.de>
2026-03-29 20:41:20 +0200

    Hope 5

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,8 @@ makedocs(format=Documenter.HTML(), modules=[GCATBase], sitename="GCATBase.jl",
     authors="Markus Gumbel and other contributors.")
 deploydocs(
     repo="github.com/cammbio/GCATBase.jl.git",
-    push_preview=true,
-    deps=nothing,
-    make=nothing
+    devbranch = "main",  # or "master", depending on your default branch
+    push_preview=false,
+    #deps=nothing,
+    #make=nothing
 )
